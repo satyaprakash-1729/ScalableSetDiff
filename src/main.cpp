30125a6299bffcbe34f8f56ed8498809552fdf2f
Satya Prakash <prakashsatya@g.ucla.edu>
2020-05-29 00:08:51 -0700

    Added recursive hashing

--- src/main.cpp
+++ src/main.cpp
@@ -46,52 +46,35 @@ public:
     }
 };
 
-int hashFunction1(int key, int N){
-    return key % N;
+int hashFunctionK(int key, int N) {
+    hash<int> h;
+    return h(key+1)%N;
 }
 
-int hashFunction2(int key, int N) {
-    return ((key >> 8 & key << 24)^(key >> 24 & key << 8)) % N;
+int hashFunctionC(int key, int N) {
+    hash<int> h;
+    unsigned long hval = h(key+1);
+    return (hval>>32 & hval>>16 & hval)%N;
 }
 
-int hashFunction3(int key, int N){
-    return ((key >> 4 & key << 8)^(key >> 8 & key << 4)) % N;
-}
-
-int hashFunction4(int key, int N) {
-    long l = 2654435769L;
-    return (key * l >> 32) % N ;
-//    return ((key >> 16 & key << 16)^(key >>  & key << 8)) % N;
-}
-
-int getMinHash(unordered_set<int>& numSet, hashFuncType hashFn){
-    int minHash = INT_MAX, temp;
-    int ans = 0;
-    for(int x: numSet){
-        if(minHash > (temp = hashFn(x, INT_MAX))){
-            minHash = temp;
-            ans = x;
+vector<int> getDiffHashedInd(int num, int cnt, int N){
+    unordered_set<int> inds;
+    for(int i=0; i<cnt; i++){
+        int ind = hashFunctionK(num, N);
+        while(inds.count(ind)){
+            ind = hashFunctionK(ind, N);
         }
+        inds.insert(ind);
     }
-    return ans;
-}
-
-int estimateSetDiffSize(unordered_set<int>& A, unordered_set<int>& B, vector<hashFuncType>& hashes){
-    int m = 0;
-    for(hashFuncType& hashFn: hashes){
-        if(getMinHash(A, hashFn) == getMinHash(B, hashFn)) m++;
-    }
-    float k = hashes.size();
-    float r = float(m)/k;
-    return ((1.-r)/(1.+r))*(float(A.size()+B.size()));
+    return vector<int>(inds.begin(), inds.end());
 }
 
 void encode(unordered_set<int>& numSet, int N,
-        vector<hashFuncType>& hashes, hashFuncType Hc,
+        int k, hashFuncType Hc,
         vector<IBFCell*>& ibf){
     for(int num : numSet) {
-        for(auto & hashe : hashes) {
-            int idx = hashe(num, N);
+        vector<int> idxs = getDiffHashedInd(num, k, N);
+        for(int idx: idxs){
             ibf[idx]->addEntry(num, Hc(num, N));
         }
     }
@@ -113,7 +96,7 @@ bool isPure(vector<IBFCell*>& diff, hashFuncType Hc, int i, int N){
 }
 
 bool decode(vector<IBFCell*>& diff, unordered_set<int>& diff_A_B,
-        unordered_set<int>& diff_B_A, vector<hashFuncType>& hashes,
+        unordered_set<int>& diff_B_A, int k,
         hashFuncType Hc, int N){
     queue<int> pureList;
     for(int i=0; i<N; i++){
@@ -135,9 +118,8 @@ bool decode(vector<IBFCell*>& diff, unordered_set<int>& diff_A_B,
         }else{
             diff_B_A.insert(idSum);
         }
-
-        for(auto & hashe : hashes) {
-            int idx = hashe(idSum, N);
+        vector<int> idxs = getDiffHashedInd(idSum, k, N);
+        for(int idx: idxs){
             diff[idx]->setIdSum(diff[idx]->getIdSum()^idSum);
             diff[idx]->setHashSum(diff[idx]->getHashSum()^Hc(idSum, N));
             diff[idx]->setCount(diff[idx]->getCount()-count);
@@ -146,21 +128,21 @@ bool decode(vector<IBFCell*>& diff, unordered_set<int>& diff_A_B,
         }
     }
     for(int i=0; i<N; i++){
-        if(diff[i]->getCount() || diff[i]->getIdSum() || diff[i]->getHashSum()) return false;
+        if(diff[i]->getCount()!=0 || diff[i]->getIdSum()!=0 || diff[i]->getHashSum()!=0) return false;
     }
     return true;
 }
 
-unordered_set<int> getSetDifference(unordered_set<int>& A,
-        unordered_set<int>& B, int N, vector<hashFuncType>& hashes,
+vector<unordered_set<int>> getSetDifference(unordered_set<int>& A,
+        unordered_set<int>& B, int N, int k,
         hashFuncType Hc){
     vector<IBFCell*> ibf1(N);
     vector<IBFCell*> ibf2(N);
     for (int i=0; i<N; i++) ibf1[i] = new IBFCell();
     for (int i=0; i<N; i++) ibf2[i] = new IBFCell();
 
-    encode(A, N, hashes, Hc, ibf1);
-    encode(B, N, hashes, Hc, ibf2);
+    encode(A, N, k, Hc, ibf1);
+    encode(B, N, k, Hc, ibf2);
 
     vector<IBFCell*> diff(N);
     subtract(ibf1, ibf2, diff, N);
@@ -168,21 +150,79 @@ unordered_set<int> getSetDifference(unordered_set<int>& A,
     unordered_set<int> diff_A_B;
     unordered_set<int> diff_B_A;
 
-    bool stat = decode(diff, diff_A_B, diff_B_A, hashes, Hc, N);
+    bool stat = decode(diff, diff_A_B, diff_B_A, k, Hc, N);
     if(!stat){
         cout<<"Error finding difference... All differences might not be recorded.\n";
     }
 
-    unordered_set<int> ans;
-    ans.insert(diff_A_B.begin(), diff_A_B.end());
-    ans.insert(diff_B_A.begin(), diff_B_A.end());
+    vector<unordered_set<int>> ans;
+    ans.push_back(diff_A_B);
+    ans.push_back(diff_B_A);
 
     return ans;
 }
 
+class Strata_IBF
+{
+public:
+    vector<vector<IBFCell*>> ibfs;
+    int ibfSize;
+    int logu;
+    int numHashes;
+
+    Strata_IBF(int ibfSize, int numHashes, int logu){
+        this->logu = logu;
+        this->ibfSize = ibfSize;
+        this->numHashes = numHashes;
+        for(int i=0; i<logu; i++){
+            vector<IBFCell*> ibf1(ibfSize);
+            for(int j=0; j<ibfSize; j++)
+                ibf1[j] = new IBFCell();
+            this->ibfs.push_back(ibf1);
+        }
+    }
+
+    int getNumTrailingZeros(int x){
+        int ans = 0;
+        while(!x%2){
+            x = x>>1;
+            ans++;
+        }
+        return ans;
+    }
+
+    void encode(unordered_set<int>& numSet, hashFuncType Hc){
+        for(int num: numSet){
+            vector<int> idxs = getDiffHashedInd(num, this->numHashes, this->ibfSize);
+            int trails = getNumTrailingZeros(num);
+            for(int idx: idxs){
+                this->ibfs[trails][idx]->addEntry(num, Hc(num, this->ibfSize));
+            }
+        }
+    }
+
+    int estimateLength(Strata_IBF* strata1, hashFuncType Hc){
+        int ans = 0;
+        for(int i=this->logu-1; i>=0; i--){
+            vector<IBFCell*> diff(this->ibfSize);
+            subtract(this->ibfs[i], strata1->ibfs[i], diff, this->ibfSize);
+            unordered_set<int> s1;
+            unordered_set<int> s2;
+            if(decode(diff, s1, s2, this->numHashes, Hc, this->ibfSize)){
+                ans += (s1.size() + s2.size());
+            }else{
+                return (1<<(i+1))*ans;
+            }
+        }
+        return ans;
+    }
+};
+
 int main(int argc, char *argv[]) {
-    unordered_set<int> A = {40,12,59,32,74,32,52,7,89,43,75,67,88,112};
-    unordered_set<int> B = {52,7,89,546,71,67,88,165,40,12,59,32,93,32};
+    unordered_set<int> A = {401,12,59,32,74,39,52,7,89,43,75,67,488,112};
+    unordered_set<int> B = {52,7,89,546,71,67,88,165,40,12,59,39,93,32};
+
+    int k = 3;
 
     /* //These two data sets produce an incorrect set difference
     unordered_set<int> A = {40,12,59,32,74,32,52,7,89,43,75,67,88,112};
@@ -239,27 +279,32 @@ int main(int argc, char *argv[]) {
     unordered_set <int> B {0,1,2,3,4,5,6};
     */
 
-    vector<hashFuncType> hashes;
-    hashes.push_back(hashFunction1);
-    hashes.push_back(hashFunction2);
-    hashes.push_back(hashFunction3);
+    hashFuncType Hc = &hashFunctionC;
 
     cout<<"Calculating estimated set difference size...\n";
-    int d = estimateSetDiffSize(A, B, hashes);
+    Strata_IBF* strata1 = new Strata_IBF(80, k, 10);
+    Strata_IBF* strata2 = new Strata_IBF(80, k, 10);
+    strata1->encode(A, Hc);
+    strata2->encode(B, Hc);
+    int d = strata1->estimateLength(strata2, Hc);
     cout<<"Estimated Set Diff Size: "<<d<<endl;
 
-    float alpha = 1.4;
-
-    hashFuncType Hc = &hashFunction4;
+    float alpha = 1.5;
 
     int N = d*alpha;
-
     cout<<"Calculating Set Difference ...\n";
-    unordered_set<int> setDiff = getSetDifference(A, B, N, hashes, Hc);
-    cout<<"---- SET DIFFERENCE ----\n";
-    for(int x : setDiff){
+    vector<unordered_set<int>> setDiff = getSetDifference(A, B, 1000, k, Hc);
+    cout<<"---- SET DIFFERENCE A-B ----\n";
+    for(int x : setDiff[0]){
         cout<<x<<endl;
     }
-
+    cout<<"---- SET DIFFERENCE B-A ----\n";
+    for(int x : setDiff[1]){
+        cout<<x<<endl;
+    }
+    unordered_set<int> diff;
+    set_difference(A.begin(), A.end(), B.begin(),
+                   B.end(), std::inserter(diff, diff.begin()));
+    cout<<"Actual Difference Size: "<<diff.size()<<endl;
     return 0;
 }
