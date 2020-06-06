54a8890fec60252ca4cbe8134685e11c9e3e123e
Satya Prakash <prakashsatya@g.ucla.edu>
2020-06-05 17:43:12 -0700

    Fixed Strata Bug

--- src/main.cpp
+++ src/main.cpp
@@ -201,18 +201,21 @@ public:
 
     static int getNumTrailingZeros(int x){
         if(!x) return 0;
-        int ans = 0;
-        while(!x%2){
-            x = x>>1;
-            ans++;
-        }
-        return ans;
+        int mask = 1;
+        int i = 0;
+	    while (mask <= x){
+	        if ((x & mask) != 0)
+	            return i;
+	        i++;
+	        mask = mask<<1;
+	    }
+        return i;
     }
 
     void encode(unordered_set<int>& numSet, hashFuncType Hc){
         for(int num: numSet){
             vector<int> idxs = getDiffHashedInd(num, this->numHashes, this->ibfSize);
-            int trails = getNumTrailingZeros(num);
+            int trails = getNumTrailingZeros(Hc(num, INT_MAX));
             for(int idx: idxs){
                 this->ibfs[trails][idx]->addEntry(num, Hc(num, this->ibfSize));
             }
@@ -240,8 +243,8 @@ int main(int argc, char *argv[]) {
     unordered_set<int> A;
     unordered_set<int> B;
 
-    A = getRandomSet(11, 100, 300);
-    B = getRandomSet(11, 100, 300);
+    A = getRandomSet(6, 100, 300);
+    B = getRandomSet(6, 100, 300);
 
     cout << "ALPHA: " << ALPHA << " BETA: " << BETA << " NUM HASHES: " << NUM_HASHES << endl;
     printSet("A", A);
